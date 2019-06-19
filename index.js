f74e10c07f036ca9790f2131cdd65e8661c07977
Alexnder <opensunset@gmail.com>
2019-06-19 17:01:32 +0300

    remove trash files, fix dependenties

--- index.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line func-names
-(async function() {
-  await new Promise(resolve => {
-    console.log('hi')
-    setTimeout(resolve, 1000)
-  })
-
-  console.log('bye')
-}())
