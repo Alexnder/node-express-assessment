91c83fc2a565e75b8c2a0966b16367c790bfd8e5
Alexnder <opensunset@gmail.com>
2019-06-19 14:47:27 +0300

    use async method in server

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express')
+const util = require('./util')
 
 const app = express()
 app.get('/', (req, res) => res.send('Hello'))
+app.get('/sum', async (req, res) => {
+  const result = await util.asyncSum(2, 2)
+  res.send(`Result: ${result}`)
+})
 app.listen(3000)
