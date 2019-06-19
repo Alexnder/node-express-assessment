c85753b42a5148805c381661609fc34600421c2c
Alexnder <opensunset@gmail.com>
2019-06-19 15:48:25 +0300

    add api for getting users

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const express = require('express')
-const util = require('./util')
+import express from 'express'
+import { getById } from './users'
 
 const app = express()
 app.get('/', (req, res) => res.send('Hello'))
-app.get('/sum', async (req, res) => {
-  const result = await util.asyncSum(2, 2)
-  res.send(`Result: ${result}`)
+app.get('/api/user/:userId', async (req, res) => {
+  try {
+    res.json(await getById(req.params.userId))
+  } catch (e) {
+    console.log(e)
+    res.status(500).json({ error: 'internal error' })
+  }
 })
+
 app.listen(3000)
---
+++ users.js
@@ -0,0 +1,18 @@
+import api from './api'
+
+// eslint-disable-next-line func-names
+async function getById(id) {
+  try {
+    const response = await api.get(`users/${id}`)
+    if (!response.data) {
+      throw new Error('broken response')
+    }
+
+    return response.data
+  } catch (e) {
+    console.log('getById error:', e)
+    throw new Error('cannot access api')
+  }
+}
+
+export { getById, getById as getByIdTest }
