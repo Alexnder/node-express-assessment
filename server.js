6f49001f21129704859a2fd6d13bafa8dfda62bd
Alexnder <opensunset@gmail.com>
2019-06-19 16:01:18 +0300

    add avatar stub

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import axios from 'axios'
 import { getById } from './users'
 
 const app = express()
@@ -6,6 +7,18 @@ app.get('/', (req, res) => res.send('Hello'))
 app.get('/api/user/:userId', async (req, res) => {
   try {
     res.json(await getById(req.params.userId))
+  } catch (e) {
+    console.log(e)
+    res.status(500).json({ error: 'internal error' })
+  }
+})
+app.get('/api/user/:userId/avatar', async (req, res) => {
+  try {
+    const user = await getById(req.params.userId)
+    const avatarUrl = user.data.avatar
+    const response = await axios.get(avatarUrl, { responseType: 'arraybuffer' })
+    const imageBase64 = Buffer.from(response.data).toString('base64')
+    res.json(imageBase64)
   } catch (e) {
     console.log(e)
     res.status(500).json({ error: 'internal error' })
